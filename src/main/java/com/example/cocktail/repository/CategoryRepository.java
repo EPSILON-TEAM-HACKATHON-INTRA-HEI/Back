d4bac441eb4858dce952928f257edc3495460621
njaina <98956159+njaina@users.noreply.github.com>
2022-08-22 07:32:33 +0300

    Delete src directory

--- src/main/java/com/example/cocktail/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.cocktail.repository;
-
-import com.example.cocktail.model.Categories;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoryRepository extends JpaRepository<Categories,Integer> {
-}
